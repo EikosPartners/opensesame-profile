047d704e55ee567a65f76128787771f80e0e2d72
Peter Fatyga <peter.fatyga@eikospartners.com>
2016-06-24 09:08:20 -0400

    Adds opensesame to project

--- app.js
+++ app.js
@@ -4,23 +4,44 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var opensesame = require('opensesame');
+var JsonStorageService = require('./services/JsonStorageService');
+
+var userService = new JsonStorageService('users.json');
 
 var user = require('./routes/user');
 var role = require('./routes/role');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app = opensesame({
+  secret: 'testSecret',
+  checkUser: (userObject, callback) => {
+    userService.get(userObject.user, (err, user) => {
+      if(err) {
+        return callback(err);
+      } else {
+        return callback(null, user);
+      }
+    });
+  },
+  registerUser: (userObject, callback) => {
+    userService.create(userObject.user, userObject, (err, user) => {
+      if(err) {
+        return callback(err);
+      } else {
+        return callback(null, userObject);
+      }
+    });
+  },
+  httpsOnly: false
+}, app);
 
 app.use('/profile/user', user);
 app.use('/profile/role', role);
@@ -39,10 +60,6 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
   });
 }
 
@@ -50,10 +67,6 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
 });
 
 
